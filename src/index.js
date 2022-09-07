f23142072614c571eca0e86f07e2fc513afbe940
zbl14 <ifthereisoneday@gmail.com>
2022-09-07 12:20:39 -0700

    adjust template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
 
-const App = () => {
-  return <React.Fragment></React.Fragment>;
-};
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
