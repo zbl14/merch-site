23f5f58290e5a26adb0090a1ac760c857977fb5b
zbl14 <ifthereisoneday@gmail.com>
2022-09-07 13:33:59 -0700

    add form

--- src/components/NewPurchaseForm.js
+++ src/components/NewPurchaseForm.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
-const NewPurchaseForm = () => {};
+const NewPurchaseForm = () => {
+  const handleNewPurchaseFormSubmission = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleNewPurchaseFormSubmission}>
+        <input type="text" name="name" placeholder="Merch Name" />
+        <input type="number" name="amount" placeholder="Purchase Amount" />
+        <button type="submit">Purchase</button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+NewPurchaseForm.propTypes = {
+  onNewFormCreation: PropTypes.func,
+};
 
 export default NewPurchaseForm;
